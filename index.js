2b99b378f3ee2d76d789af56c2bbeae8d1c1ba58
Jiuhong <jiuhong@casperlabs.io>
2022-07-02 11:49:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!\n')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
